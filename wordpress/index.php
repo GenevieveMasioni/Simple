45d9c689f65e0e4bf42f262a45df51855689eeba
Genevieve Masioni <masionigenevieve@gmail.com>
2018-08-03 18:47:38 +0200

    IMPLEMENT ARTICLES DISPLAY ON BLOG PAGE
    Displays all blog posts on the blog page with thumbnail, title, category, date, author, comment and excerpt

--- wordpress/index.php
+++ wordpress/index.php
@@ -1,71 +1,58 @@
 <?php get_header(); ?>
 <section class="content">
-  <article class="blogpost">
-    <img src="assets/images/thumbnail.png" alt="">
-    <h2 class="blogpost-title">
-      Adaptive Vs. Responsive Layouts And Optimal Text Readability
-    </h2>
-    <section class="blogpost-meta-container">
-      <ul class="blogpost-meta">
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">February 2, 2018</a></li>
-        <li><a href="#">Albert Einstein</a></li>
-        <li><a href="#">4 comments</a></li>
-      </ul>
-    </section>
-    <p class="blogpost-excerpt">
-      Responsive web design offers us a way forward, finally allowing us
-      to design for the ebb and flow of things. There are many variations
-      of passages of Lorem Ipsum available, but the majority have suffered
-      alteration in some form, by injected humour, or randomised words
-      which don’t look even slightly.
-    </p>
-    <a href="#" class="blogpost-readmore">Continue reading <i class="fa fa-arrow-right"></i></a>
-  </article>
-  <article class="blogpost">
-    <img src="assets/images/thumbnail.png" alt="">
-    <h2 class="blogpost-title">
-      Adaptive Vs. Responsive Layouts And Optimal Text Readability
-    </h2>
-    <section class="blogpost-meta-container">
-      <ul class="blogpost-meta">
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">February 2, 2018</a></li>
-        <li><a href="#">Albert Einstein</a></li>
-        <li><a href="#">4 comments</a></li>
-      </ul>
-    </section>
-    <p class="blogpost-excerpt">
-      Responsive web design offers us a way forward, finally allowing us
-      to design for the ebb and flow of things. There are many variations
-      of passages of Lorem Ipsum available, but the majority have suffered
-      alteration in some form, by injected humour, or randomised words
-      which don’t look even slightly.
-    </p>
-    <a href="#" class="blogpost-readmore">Continue reading <i class="fa fa-arrow-right"></i></a>
-  </article>
-  <article class="blogpost">
-    <img src="assets/images/thumbnail.png" alt="">
-    <h2 class="blogpost-title">
-      Adaptive Vs. Responsive Layouts And Optimal Text Readability
-    </h2>
-    <section class="blogpost-meta-container">
-      <ul class="blogpost-meta">
-        <li><a href="#">Web Design</a></li>
-        <li><a href="#">February 2, 2018</a></li>
-        <li><a href="#">Albert Einstein</a></li>
-        <li><a href="#">4 comments</a></li>
-      </ul>
-    </section>
-    <p class="blogpost-excerpt">
-      Responsive web design offers us a way forward, finally allowing us
-      to design for the ebb and flow of things. There are many variations
-      of passages of Lorem Ipsum available, but the majority have suffered
-      alteration in some form, by injected humour, or randomised words
-      which don’t look even slightly.
-    </p>
-    <a href="#" class="blogpost-readmore">Continue reading <i class="fa fa-arrow-right"></i></a>
-  </article>
+  <?php if(have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <article class="blogpost">
+      <?php if(has_post_thumbnail()) : ?>
+        <?php the_post_thumbnail(); ?>
+      <?php endif; ?>
+      <h2 class="blogpost-title">
+        <?php the_title(); ?>
+      </h2>
+      <section class="blogpost-meta-container">
+        <ul class="blogpost-meta">
+          <li>
+            <?php
+            $categories = get_the_category();
+            $separator = ', ';
+            $output = '';
+
+            if($categories) {
+              foreach ($categories as $category) {
+                $output .=
+                '<a href="'.get_category_link(
+                  $category->term_id).'">'
+                  .$category->cat_name.'</a>'.$separator;
+              }
+            }
+            echo trim($output, $separator);
+             ?>
+          </li>
+          <li><?php the_time('F j, Y'); ?></li>
+          <li>
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+              <?php the_author(); ?>
+            </a>
+          </li>
+          <li>
+            <a href="<?php comments_link(); ?>">
+            <?php comments_number( 'no comment', 'one comment', '% comments' ); ?>
+          </a>
+        </li>
+        </ul>
+      </section>
+      <p class="blogpost-excerpt">
+        <?php the_excerpt(); ?>
+      </p>
+      <a href="<?php the_permalink(); ?>" class="blogpost-readmore">
+        <?php echo __('Continue reading'); ?>
+        <i class="fa fa-arrow-right"></i>
+      </a>
+    </article>
+  <?php endwhile; ?>
+<?php else : ?>
+  <p>No posts to display yet.</p>
+<?php endif; ?>
 </section>
 <section class="sidebar">
   <section class="widget">
